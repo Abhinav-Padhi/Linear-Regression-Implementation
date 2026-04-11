27201bd166940955913c928afb73a21b04164328
Abhinav-Padhi <abhinavpadhi1@gmail.com>
2026-04-11 19:04:10 +0000

    Implemented Stochastic Gradient Descent

--- src/ML.py
+++ src/ML.py
@@ -1,6 +1,7 @@
 import GradientDescent
 import MiniBatch
 import pandas as pd
+import Stochastic
 df = pd.read_csv('https://github.com/ybifoundation/Dataset/raw/main/Salary%20Data.csv')
 x = df['Experience Years']
 y = df['Salary']
@@ -12,3 +13,8 @@ mb = MiniBatch.MiniBatch(x,y,1,0,0,alpha=0.001)
 mb_ans = mb.update(standardized=True)
 
 print(f"{mb_ans[0]:.4f}, {mb_ans[1]:.4f}")
+
+st = Stochastic.Stochastic(x,y,0,0)
+st_ans = st.update(standardized=False)
+
+print(f"{st_ans[0]:.4f}, {st_ans[1]:.4f}")
--- src/Stochastic.py
+++ src/Stochastic.py
@@ -1,5 +1,6 @@
+import random
 class Stochastic:
-    def __init__(self,x,y,w,b,alpha=0.01,epochs=10000):
+    def __init__(self,x,y,w,b,alpha=0.001,epochs=1000):
         self.x = x
         self.y = y
         self.w = w
@@ -9,5 +10,20 @@ class Stochastic:
     
     def update(self,standardized=True):
         n = len(self.x)
-        pass
+        if standardized==True:
+            mean_x = sum(xi for xi in self.x)/n                        #mean of x
+            std_x = (sum((xi-mean_x)**2 for xi in self.x)/n)**0.5      #standard deviation of x
+            self.x = [(xi-mean_x)/std_x for xi in self.x]              #normalizing x
+            mean_y = sum(yi for yi in self.y)/n                        #mean of y
+            std_y = (sum((yi-mean_y)**2 for yi in self.y)/n)**0.5      #standard deviation of y
+            self.y = [(yi-mean_y)/std_y for yi in self.y]              #normalizing y
+        error = []
+        for i in range(self.epochs):
+            data = list(zip(self.x,self.y))
+            random.shuffle(data)
+            for X,Y in data:
+                e = -Y + self.b + self.w*X
+                self.b = self.b - 2*self.alpha*e 
+                self.w = self.w - 2*self.alpha*e*X
+        return [self.w,self.b]
         
